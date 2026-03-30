909d20b186b5165bc5d14378bc84b3a48bee1ac5
Marsels <ipb22.m.strikaitis@vtdt.edu.lv>
2026-03-30 16:09:42 +0200

    Mini izmaiņas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import SpotlightWrapper from "./SpotlightWrapper";
 export default function Home() {
   return (
     <SpotlightWrapper>
-      <h1>Mans Portfolio!</h1>
-      <p>Seko manam darbam 🚀</p>
+      <h1>Mans Portfolio</h1>
+      <p>Seko manam darbam!!!</p>
     </SpotlightWrapper>
   );
 }
